97dca27ccde47930563a13417dfaaaaccba0ffbb
James W. McClain <jmcclain@azavea.com>
2019-10-31 09:35:38 -0400

    Local and AWS Changes

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-ln /tmp/deeplab_"$1".pth /tmp/deeplab.pth
+MODEL=$1
 shift
 
-aws s3 cp "$1" /tmp/mul.tif
+aws s3 cp $1 /tmp/image.tif
 shift
 
-/scripts/deeplab_inference.py --architecture resnet18-binary --classes 2 --bands 1 2 3 4 5 6 7 8 9 10 11 12 --libchips ??? --model ??? --inference-img ???
+OUTPUT_PREFIX=$1
+shift
 
 mkdir -p /tmp/output /tmp/input
-
-OUTPUT_PREFIX = $1
-shift
-
 for uri in $*
 do
     aws s3 cp $uri /tmp/input/
 done
 
-/scripts/elaborate.py --raster /tmp/pred.tif --output-prefix /tmp/output/ --geojson $(ls /tmp/input/*.*json)
+/scripts/local.sh $MODEL /tmp/image.tif /tmp/output/output- $(ls /tmp/input/*.*json)
 
 aws s3 sync /tmp/output/ $OUTPUT_PREFIX
