e1469bc9fb03fe639f695370a4ba47653224567a
James W. McClain <jmcclain@azavea.com>
2020-07-29 19:43:34 -0400

    Reader Thread

---
+++ src/libchips/macros.h
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ * =====================
+ *
+ * Copyright © 2019-2020 Azavea
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#ifndef __MACROS_H__
+#define __MACROS_H__
+
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#define UNLOCK(index, useconds)                     \
+    {                                               \
+        pthread_mutex_unlock(&slot_mutexes[index]); \
+        usleep(useconds);                           \
+    }
+#define UNLOCK_BREAK(index, useconds) \
+    {                                 \
+        UNLOCK(index, useconds)       \
+        break;                        \
+    }
+#define UNLOCK_CONTINUE(index, useconds) \
+    {                                    \
+        UNLOCK(index, useconds)          \
+        continue;                        \
+    }
+
+#define EMPTY_WINDOW (GDAL_DATA_COVERAGE_STATUS_EMPTY & GDALGetDataCoverageStatus(imagery_first_bands[id], window_size * x_offset, window_size * y_offset, window_size, window_size, 0, NULL))
+#define BAD_WINDOW (x_offset < 0 || x_offset > ((widths[id] / window_size) - 1) || y_offset < 0 || y_offset > ((heights[id] / window_size) - 1))
+#define BAD_TRAINING_WINDOW (((x_offset + y_offset) % 7) == 0)
+#define BAD_EVALUATION_WINDOW (((x_offset + y_offset) % 7) != 0)
+
+#endif
---
+++ src/libchips/reader.c
@@ -0,0 +1,203 @@
+/*
+ * The MIT License (MIT)
+ * =====================
+ *
+ * Copyright © 2019-2020 Azavea
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdint.h>
+
+#include <gdal.h>
+
+#include "globals.h"
+#include "reader.h"
+#include "macros.h"
+
+/**
+ * The code behind the reader threads.
+ *
+ * @param _id The id of this particular thread
+ * @return Unused
+ */
+void *reader(void *_id)
+{
+    uint64_t id = (uint64_t)_id;
+    int x_offset = 0;
+    int y_offset = 0;
+    int slot = -1;
+    CPLErr err = CE_None;
+    unsigned int state = (unsigned long)id;
+
+    while (operation_mode == training || operation_mode == evaluation)
+    {
+        // Find an unused data slot
+        for (slot = rand_r(&state) % M; (operation_mode == training || operation_mode == evaluation); slot = (slot + 1) % M)
+        {
+            // If slot is unlocked and slot is empty, read
+            if ((pthread_mutex_trylock(&slot_mutexes[slot]) == 0) && (ready[slot] == 0))
+            {
+                goto read_things;
+            }
+            UNLOCK_CONTINUE(slot, 100);
+        }
+
+        // If search for slot terminated because the mode changed,
+        // break out of the loop
+        if (operation_mode != training && operation_mode != evaluation)
+        {
+            UNLOCK_BREAK((slot + M - 1) % M, 0);
+        }
+
+    read_things:
+
+#if defined(CHAMPION_EDITION)
+        for (int i = 0; i < (1 << 7); ++i)
+#else
+        for (int i = 0; i < 1; ++i)
+#endif
+        {
+            int wradius = radius / window_size;
+
+            // Get a suitable training or evaluation window
+            if (operation_mode == training) // Training chip
+            {
+                x_offset = y_offset = -1;
+                while (BAD_WINDOW || BAD_TRAINING_WINDOW || EMPTY_WINDOW)
+                {
+                    const int rand_x = rand_r(&state) % (2 * wradius);
+                    const int rand_y = rand_r(&state) % (2 * wradius);
+                    x_offset = center_xs[id] + rand_x - wradius;
+                    y_offset = center_ys[id] + rand_y - wradius;
+                }
+            }
+            else if (operation_mode == evaluation) // Evaluation chip
+            {
+                x_offset = y_offset = -1;
+                while (BAD_WINDOW || BAD_EVALUATION_WINDOW || EMPTY_WINDOW)
+                {
+                    const int rand_x = rand_r(&state) % (2 * wradius);
+                    const int rand_y = rand_r(&state) % (2 * wradius);
+                    x_offset = center_xs[id] + rand_x - wradius;
+                    y_offset = center_ys[id] + rand_y - wradius;
+                }
+            }
+            else
+            {
+                break;
+            }
+
+            x_offset *= window_size;
+            y_offset *= window_size;
+
+            // Read imagery
+            pthread_mutex_lock(&dataset_mutexes[id]);
+            err = GDALDatasetRasterIO(imagery_datasets[id], 0,
+                                      x_offset, y_offset, window_size, window_size,
+                                      imagery_slots[slot],
+                                      window_size, window_size,
+                                      imagery_data_type, band_count, bands,
+                                      0, 0, 0);
+            pthread_mutex_unlock(&dataset_mutexes[id]);
+            if (err != CE_None)
+            {
+                fprintf(stderr, "FAILED IMAGERY READ AT %d %d\n", x_offset, y_offset);
+                UNLOCK_CONTINUE(slot, 1000)
+            }
+
+            // Read labels
+            if (label_datasets[id] != NULL)
+            {
+                err = GDALDatasetRasterIO(label_datasets[id], 0,
+                                          x_offset, y_offset, window_size, window_size,
+                                          label_slots[slot],
+                                          window_size, window_size,
+                                          label_data_type, 1, NULL,
+                                          0, 0, 0);
+                if (err != CE_None)
+                {
+                    fprintf(stderr, "FAILED LABEL READ AT %d %d\n", x_offset, y_offset);
+                    UNLOCK_CONTINUE(slot, 1000)
+                }
+            }
+
+#if defined(CHAMPION_EDITION)
+            // Imagery and labels have been read, at this point.  Now
+            // see if they have the desired characteristics.  If not,
+            // continue.
+            int should_go_again = 0;
+
+            // Check to ensure that zero is not present in the labels
+            // (that value is forbidden) and 2 is present (as
+            // desired).
+            uint32_t label_word_desired = 0;
+            for (int j = 0; j < 1 * window_size * window_size; ++j)
+            {
+                uint32_t word = ((uint32_t *)(label_slots[slot]))[j];
+                if (!word)
+                {
+                    should_go_again |= 1;
+                }
+                label_word_desired |= word;
+            }
+            if (should_go_again)
+            {
+                continue;
+            }
+
+#if 0
+            // Check ensure that IEEE-754 zero is not present in the
+            // imagery (the value is forbidden).
+            for (int j = 0; j < band_count * window_size * window_size; ++j)
+            {
+                uint32_t word = ((uint32_t *)imagery_slots[slot])[j];
+                if (!(word & 0x7fffffff))
+                {
+                    should_go_again |= 1;
+                }
+            }
+            if (should_go_again)
+            {
+                continue;
+            }
+#endif
+
+            // Check to see whether label value 2 was found earlier.
+            // If so, this pair is okay.  If not, fall through and
+            // (probably) do the for-loop again.
+            if (0x02 & label_word_desired)
+            {
+                break;
+            }
+#endif
+        }
+
+        // The slot is now ready for reading
+        ready[slot] = 1;
+
+        // Done
+        UNLOCK(slot, 1000)
+    }
+
+    return NULL;
+}
